1058e56d57470a0f1a814375c359bda336b53cbe
fidjumbo <fidatemie@gmail.com>
2026-03-08 14:13:25 +0100

    Fix issue in countdown code

--- frontend/src/components/EventForm.jsx
+++ frontend/src/components/EventForm.jsx
@@ -21,25 +21,30 @@ const EventForm = ({
         value={name}
         onChange={(e) => setName(e.target.value)}
         required
-        className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300"
+        className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 
+        focus:ring-blue-400 transition duration-300"
       />
       <input
         type="datetime-local"
         value={date}
         onChange={(e) => setDate(e.target.value)}
         required
-        className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300"
+        placeholder="Date/Time"
+        className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 
+        focus:ring-blue-400 transition duration-300"
       />
       <input
         type="text"
         placeholder="Description (optional)"
         value={description}
         onChange={(e) => setDescription(e.target.value)}
-        className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300"
+        className="p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 
+        focus:ring-blue-400 transition duration-300"
       />
       <button
         type="submit"
-        className="py-3 px-5 bg-blue-600 text-white font-bold rounded-md hover:bg-blue-500 transition duration-300"
+        className="py-3 px-5 bg-blue-600 text-white font-bold rounded-md hover:bg-blue-500 
+        transition duration-300"
       >
         {editingId ? "Update Event" : "Add Event"}
       </button>
